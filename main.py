71c293d636e8404d2b2806c6bd2550f44cc2b8a1
Sebastian <ippo_sebas@hotmail.com>
2022-12-15 08:35:30 -0500

    Add comments to better understanding and fix new problems

--- main.py
+++ main.py
@@ -24,30 +24,29 @@ cup_de_gas = []
 
 class webScrapping:
     def recolectar_info(self):
-        for i in range(5):
+        for i in range(2):
             try:
-                driver.find_element(By.XPATH, "//input[@id='cups']").send_keys(cup[i])
+                driver.find_element(By.XPATH, "//body/div/section[@role='main']/div/div/div/div/div/div/div/div/div/div/div/div/div/input[1]").send_keys(cup[i])
                 time.sleep(2)
                 driver.find_element(By.XPATH,
-                                    "//form[@action='#']//div//div//div//div//div//div//div//button[@type='submit']").click()
+                                    "//body[1]/div[1]/section[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/button[1]").click()
                 time.sleep(40)
                 box_direccion = driver.find_element(By.XPATH,
                                                     "//body[1]/div[1]/section[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div["
-                                                    "2]/div[1]/p[1]")
+                                                    "2]/div[1]/p[1]")               #RECOGE TODO LOS VALORES DE LA INFORMACION QUE DAL EL CUP EN BOX
             except NoSuchElementException:
                 driver.refresh()
                 continue
             time.sleep(5)
-            list_direccion = box_direccion.text.split("\n")
-            list_direccion.pop()
+            list_direccion = box_direccion.text.split("\n")                 #LOS SEPARA EN UNA LISTA POR CADA ESTA QUE HAY ['CALLE BUEN RETIRO, 11 3 9', '46015 - VALÈNCIA', 'VALENCIA/VALÈNCIA', 'CUPS :ES 0021 **** **** 1068 MX']
+            list_direccion.pop()                                            #ELIMINAMOS EL VALOR DE CUP QUE NO NOS SIRVE ['CALLE BUEN RETIRO, 11 3 9', '46015 - VALÈNCIA', 'VALENCIA/VALÈNCIA']
             arr_direccion = np.array(list_direccion)
-
             calle.append(arr_direccion[0])
 
-            a, b = str(arr_direccion[1]).split("-", 1)
+            a, b = str(arr_direccion[1]).split("-", 1)                      #DIVIDIMOS LA LOVALIDAD Y EL CODIGO POSTAL
             cp.append(str(a).strip())
             localidad.append(str(b).strip())
-            provincia.append(arr_direccion[2])
+            provincia.append(arr_direccion[2])                              #AÑADIMOS LA PROVINCIA
 
             try:
                 driver.find_element(By.XPATH, "//div[@id='gas']").click()
@@ -58,8 +57,8 @@ class webScrapping:
 
             try:
                 cup_gas = driver.find_element(By.XPATH,
-                                              "//body/div[@id='root']/section[@role='main']/div/div/div/div/div[2]/p[1]").text
-                cup_de_gas.append(cup_gas[5:].strip())
+                                              "//div[@class='MuiBox-root jss81']//p[1]").text
+                cup_de_gas.append(cup_gas[5:].strip())                      #Añadimos el cup de gas :D
 
             except NoSuchElementException:
                 driver.refresh()
